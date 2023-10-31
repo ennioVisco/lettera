b1686b9a34ba7c3f7190b2172c5568e3ea6eb53d
Ennio Visconti <ennio.visconti@gmail.com>
2023-10-31 17:54:26 +0100

    build: updated deps and added fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,28 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: [
+    // ES Lint
+    '@nuxtjs/eslint-module',
     // https://nuxt.studio
     '@nuxthq/studio',
     // https://content.nuxtjs.org
     '@nuxt/content',
     // https://pinceau.dev
-    'pinceau/nuxt',
+    // 'pinceau/nuxt',
     // Google fonts module
-    '@nuxtjs/google-fonts'
+    // '@nuxtjs/google-fonts',
+    // Radix UI
+    'radix-vue/nuxt',
+    // Tailwind CSS
+    // '@nuxtjs/tailwindcss'
   ],
   content: {
     documentDriven: true
   },
-  pinceau: {
-    studio: true
-  },
+  // pinceau: {
+  //   studio: true
+  // },
   typescript: {
     includeWorkspace: true
   },
-  googleFonts: {
-    families: {
-      Lato: {
-        wght: [100, 300, 400, 700, 900],
-        ital: [400, 900]
-      }
-
-    }
-  }
 })
